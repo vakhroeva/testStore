4920df4423d92d3b35c81c8f93107cc634162d75
YuliaVakhroeva <vakhroevaylia@gmail.com>
2025-05-26 14:48:39 +0400

    added seeder for categories and some products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (Category::count() === 0){
+            $categories = ['легкий', 'средний', 'тяжелый'];
+
+            foreach ($categories as $name) {
+                Category::firstOrCreate(['name' => $name]);
+            }
+        }
+
+        if (Product::count() === 0){
+
+            $category1 = Category::where('name', 'легкий')->first();
+
+            Product::create([
+                'name' => 'Сережки',
+                'description' => 'Изящные серебряные сережки ручной работы.',
+                'category_id' => $category1->id,
+                'price' => 1499.99,
+            ]);
+
+            $category2 = Category::where('name', 'средний')->first();
+
+            Product::create([
+                'name' => 'Сапожки',
+                'description' => 'Изящные сапожки для красивой ножки.',
+                'category_id' => $category2->id,
+                'price' => 4499.99,
+            ]);
+
+            $category3 = Category::where('name', 'тяжелый')->first();
+
+            Product::create([
+                'name' => 'Шкаф',
+                'description' => 'Огромный шкаф.',
+                'category_id' => $category3->id,
+                'price' => 14499.99,
+            ]);
+        }
+
+
     }
 }
